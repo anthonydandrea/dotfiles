2789f2a89f47b9a312665056decf01ab8600e720
anthonydandrea <anthonydandrea2@gmail.com>
2020-05-10 21:20:00 -0400

    fixes

--- .scripts/polybar/launch_polybar.sh
+++ .scripts/polybar/launch_polybar.sh
@@ -1,14 +1,23 @@
 #!/bin/sh
 
-if [ $1 ]; then
-    export MONITOR=$1
-    #polybar example -q  &
-    polybar top -q  &
+#if [ $1 ]; then
+    #export MONITOR=$1
+    ##polybar example -q  &
+    #polybar top -q  &
+#else
+	#killall -q polybar
+	#for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    		#export MONITOR=$m
+    		##polybar example -q &
+    		#polybar top -q &
+	#done
+#fi
+
+
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=$m polybar --reload top &
+  done
 else
-	killall -q polybar
-	for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-    		export MONITOR=$m
-    		#polybar example -q &
-    		polybar top -q &
-	done
+  polybar --reload top &
 fi
