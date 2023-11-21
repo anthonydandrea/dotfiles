e12ec7f4ac0ef25d1d242aa76753861b5d39d060
Anthony D'Andrea <anthdand@amazon.com>
2023-11-21 17:05:56 +0000

    updates

--- .tmux/shift_windows_left.py
+++ .tmux/shift_windows_left.py
@@ -1,7 +1,10 @@
 import subprocess
 
 # Define the tmux command to shift windows to the left
-tmux_command = "tmux move-window -s {} -t {}"
+move_window_cmd = "tmux move-window -s {} -t {}"
+select_window_cmd = "tmux select-window -t :{}"
+
+cur_window = int(subprocess.check_output('tmux display-message -p "#{window_index}"', shell=True).decode('utf-8'))
 
 # Get the total number of windows in your tmux session
 raw_windows = subprocess.check_output("tmux list-windows", shell=True).decode('utf-8').splitlines()
@@ -16,19 +19,22 @@ while i < len(filled_array):
     while i < len(filled_array) and filled_array[i]:
         i += 1
     if i == len(filled_array):
-        exit(0)
+        break
 
     j = i + 1
     while j < len(filled_array) and not filled_array[j]:
         j += 1
 
     if j == len(filled_array):
-        exit(0)
+        break
 
     filled_array[i] = True
     filled_array[j] = False
-    # print(f'Move {j} to {i}')
+    if j == cur_window:
+        print(f"Set cur_window from {cur_window} to {i}")
+        cur_window = i
 
-    subprocess.run(tmux_command.format(j, i), shell=True)
+    subprocess.run(move_window_cmd.format(j, i), shell=True)
 
+subprocess.run(select_window_cmd.format(cur_window), shell=True)
 exit(0)
