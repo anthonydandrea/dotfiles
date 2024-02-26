8f8d7bfdd691d1f7612c2382ccfa3a69084de346
Anthony D'Andrea <anthdand@amazon.com>
2024-02-26 16:29:06 +0000

    updates

--- .tmux/left_status.sh
+++ .tmux/left_status.sh
@@ -8,11 +8,13 @@ function main() {
 
     if [ "$os" == "Darwin" ]; then
         battery=$(pmset -g batt | egrep "([0-9]+\%).*" -o --colour=auto | cut -f1 -d';' | sed 's/%//')
+        printf "Battery: %s%s       " "$battery" "%"
     else
         battery=''
+        # Centering tmux status
+        printf "                   "
     fi
 
-    printf "Battery: %s%s       " "$battery" "%"
 }
 
 main
--- .zshrc
+++ .zshrc
@@ -88,7 +88,7 @@ export CLOUDSDK_PYTHON=/usr/local/bin/python3
 
 export PYSPARK_DRIVER_PYTHON=jupyter
 export PYSPARK_DRIVER_PYTHON_OPTS='notebook'
-export JAVA_HOME=/usr/local/opt/openjdk@20/libexec/openjdk.jdk/Contents/Home
+# export JAVA_HOME=/usr/local/opt/openjdk@20/libexec/openjdk.jdk/Contents/Home
 
 
 if [ -f '/Users/anthdand/.zshrc.aws' ] || [ -f '/home/anthdand/.zshrc.aws' ]; then
