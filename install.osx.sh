f065189ca231794eba61d6897f6eb6df0cb033b9
Anthony D'Andrea <anthdand@amazon.com>
2024-04-29 10:47:23 -0400

    updates

--- install.osx.sh
+++ install.osx.sh
@@ -26,6 +26,10 @@ ln -s ~/Repos/dotfiles/.p10k.zsh ~/.p10k.zsh
 mkdir -p ~/.tmux/plugins
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+git clone https://github.com/jeffreytse/zsh-vi-mode \
+  $ZSH_CUSTOM/plugins/zsh-vi-mode
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
 
 
 
