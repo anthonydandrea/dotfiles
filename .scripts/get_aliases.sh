87e79c5461820ce50b372e23f8dc6614afbbac60
Anthony D'Andrea <anthonydandrea2@gmail.com>
2025-01-05 15:56:40 -0800

    updates

--- .scripts/get_aliases.sh
+++
@@ -1,2 +0,0 @@
-alias | awk -F'[ =]' '{print $2}'
-alias wpr='~/.config/i3/wp.sh'
--- .scripts/sleep/sleep.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-TMPBG=/tmp/screen.png
-LOCK=$HOME/Pictures/lock.png
-#RES=1920x1080
-RES="$(xdpyinfo | awk '/dimensions/{print $2}')"
-
-ffmpeg -f x11grab -video_size $RES -y -i $DISPLAY -i $LOCK -filter_complex "boxblur=22:1,overlay=(main_w-overlay_w)/2:(main_h-overlay_h)/2" -vframes 1 $TMPBG
-sudo pm-suspend & i3lock -i $TMPBG
