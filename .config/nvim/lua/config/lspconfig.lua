751aaf70e1ea563e25dd738750aaad85d5281666
Anthony D'Andrea <anthonydandrea2@gmail.com>
2025-01-15 22:07:58 -0800

    updates

--- .config/nvim/lua/config/lspconfig.lua
+++ .config/nvim/lua/config/lspconfig.lua
@@ -2,18 +2,8 @@
 local capabilities = require('cmp_nvim_lsp').default_capabilities()
 local lspconfig = require('lspconfig')
 
-lspconfig.rust_analyzer.setup {
-    capabilities = capabilities,
-    settings = {['rust-analyzer'] = {cargo = {allFeatures = true}}}
-}
-vim.api.nvim_create_user_command('RAE', function()
-    vim.cmd('edit ~/.config/nvim/init.lua') -- Replace 'filename' with your actual file name
-    vim.cmd('/lspconfig.rust_analyzer.setup') -- Replace 'search_string' with the text to find
-end, {})
-
+lspconfig.rust_analyzer.setup {capabilities = capabilities}
 lspconfig.pyright.setup {capabilities = capabilities}
-lspconfig.ts_ls.setup {
-    -- capabilities = capabilities,
-}
+lspconfig.ts_ls.setup {capabilities = capabilities}
 lspconfig.clangd.setup {capabilities = capabilities}
 
